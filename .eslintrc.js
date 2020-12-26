2bf5ef1299878cb88a08329105cad1d30481f631
kaznak <kaznak.at.work@gmail.com>
2020-12-26 23:44:32 +0900

    fix environment

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,35 @@
 module.exports = {
-    env: {
-        browser: true
-    },
+    parser: "@typescript-eslint/parser",
     plugins: ["@typescript-eslint"],
     extends: [
         "eslint:recommended",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
         "plugin:react/recommended",
         "plugin:react-hooks/recommended",
-        "plugin:prettier/recommended",
-        "prettier/@typescript-eslint",
-        "prettier/react"
+        "plugin:@typescript-eslint/recommended",
+        "prettier",
+        "prettier/@typescript-eslint"
     ],
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true
-        },
-        project: "./tsconfig.eslint.json",
-    },
-    rules: {
-        "react/no-unknown-property": ["error", { ignore: ["class"] }],
-    },
     settings: {
         react: {
             pragma: "h",
             version: "detect"
-        },
+        }
+    },
+    env: {
+        es6: true,
+        browser: true,
+        jest: true,
+        node: true
+    },
+    rules: {
+        "react/no-unknown-property": ["error", { ignore: ["class"] }],
+        "@typescript-eslint/interface-name-prefix": "warn"
     },
     overrides: [
         {
             files: ["*.js"],
             rules: {
-                "@typescript-eslint/explicit-function-return-type": "off",
+                "@typescript-eslint/explicit-function-return-type": "off"
             }
         }
     ]
